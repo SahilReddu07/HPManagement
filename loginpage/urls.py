08d3aabd1cc9652334945ebae6e8e29e93a3bdf4
SahilReddu07 <132154037+SahilReddu07@users.noreply.github.com>
2023-05-06 19:54:42 +0530

    Update urls.py

--- loginpage/urls.py
+++ loginpage/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("hhome", views.hhome, name='Home Page'),
     path("dhome", views.dhome, name='Home Page'),
     path("refferal", views.refferal, name='Refferal Page'),
-    path("history", views.history, name='History Page')
+    path("history", views.history, name='History Page'),
+    path("thistory", views.thistory, name='''Today's History Page'''),
+    path("dhistory", views.dhistory, name='History Page')
 
 ]
